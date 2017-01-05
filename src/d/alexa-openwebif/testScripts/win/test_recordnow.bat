7e2b0c02ea292e13259bf1bcd8a9acbfc00f03a5
Fabian Wallentowitz <fabian@wallentowitz.de>
2017-01-05 19:55:52 +0100

    Windowsbuilds (#16)
    * added test scripts for windows (closes #16)

---
+++ src/d/alexa-openwebif/testScripts/win/test_recordnow.bat
@@ -0,0 +1,1 @@
+alexa-openwebif.exe true "{\"session\": {\"sessionId\": \"\",\"application\": {\"applicationId\": \"\"},\"attributes\": {},\"user\": {\"userId\": \"\"},\"new\": true},\"request\": {\"type\": \"IntentRequest\",\"requestId\": \"\",\"locale\": \"\",\"timestamp\": \"\", \"intent\":{\"name\":\"IntentRecordNow\"}},\"version\": \"\"}" "{}"
