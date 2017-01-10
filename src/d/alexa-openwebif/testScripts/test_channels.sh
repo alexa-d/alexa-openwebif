c6aa35e0e7910bfc12ca9fee0969765ab3c50779
Fabian Wallentowitz <fabian@wallentowitz.de>
2017-01-10 23:22:20 +0100

    added some new test scripts

---
+++ src/d/alexa-openwebif/testScripts/test_channels.sh
@@ -0,0 +1,1 @@
+./alexa-openwebif true "{\"session\": {\"sessionId\": \"\",\"application\": {\"applicationId\": \"\"},\"attributes\": {},\"user\": {\"userId\": \"\"},\"new\": true},\"request\": {\"type\": \"IntentRequest\",\"requestId\": \"\",\"locale\": \"\",\"timestamp\": \"\", \"intent\":{\"name\":\"IntentServices\"}},\"version\": \"\"}" "{}"
